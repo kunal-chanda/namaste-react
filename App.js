7df3974ebfadd58ab4f632c2822ae0044819185c
Kunal <kunalrcciit@gmail.com>
2023-03-23 16:13:44 +0530

    episode - 01

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1",
+    {id:"heading", xyz: "ABCD"},
+    "Namaste React Training - This is a React Course - print using React!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
